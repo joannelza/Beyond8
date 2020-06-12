d44823866ef110265876ff249b8574f39f1fa22d
Joann <joannpelza0@gmail.com>
2020-06-12 15:06:35 +0530

    added backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+var express=require("express")
+var app=express()
+
+var cors=require("cors")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+var port=process.env.PORT || 8080
+
+
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+    )
+
+const url="mongodb://localhost:27017/admin1"
+mongoose
+.connect(url,{useNewUrlParser:true})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err))
+
+var Users=require('./routes/Users')
+
+app.use('/',Users)
+
+app.listen(port, () => {
+    console.log("Server is running on port : " + port)
+})
